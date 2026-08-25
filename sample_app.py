355d3c54b16e9116938b5c5f48d4bb5a6d3cdd55
Ga192ange <angelicagarcia1908@gmail.com>
2026-08-25 14:51:32 -0500

    Cambios para DevSecOps

--- sample_app.py
+++ sample_app.py
@@ -1,26 +1,31 @@
+import os
+
 from flask import Flask, render_template
 import pymysql
 
 sample = Flask(__name__)
+
 
 @sample.route("/")
 def home():
     try:
         # Vamos a intentar conectarnos a la BD
         conn = pymysql.connect(
-        host="servidor-bd-082",
-        user="root",
-        password="sena123",
-        database="082_db",
-        connect_timeout=3
-      )
-		
+            host="servidor-bd-082",
+            user="root",
+            password=os.getenv("MYSQL_ROOT_PASSWORD"),
+            database="082_db",
+            connect_timeout=3
+        )
+
         conn.close()
-        db_status= "Conexion exitosa a la BD!"
+        db_status = "Conexion exitosa a la BD!"
+
     except Exception as e:
         db_status = f"Error en la conexion: {e}"
 
     return render_template("index.html", db_status=db_status)
 
-if __name__ == '__main__':
-    sample.run(host="0.0.0.0", port=5050, debug=True)
+
+if __name__ == "__main__":
+    sample.run(host="0.0.0.0", port=5050, debug=False)  # nosec B104
